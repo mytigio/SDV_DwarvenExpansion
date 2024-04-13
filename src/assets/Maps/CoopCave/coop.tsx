5dc85fe9a1e0d3934e566505fdca813a43e47112
Robert Howe <mytigio+github@gmail.com>
2024-04-13 15:50:10 -0600

    Add updated coop cave.

---
+++ src/assets/Maps/CoopCave/coop.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="coop" tilewidth="16" tileheight="16" tilecount="84" columns="4">
+ <image source=".coopTiles.png" width="64" height="336"/>
+ <tile id="33">
+  <properties>
+   <property name="Passable" value="0"/>
+   <property name="Trough" value="T"/>
+  </properties>
+ </tile>
+</tileset>
